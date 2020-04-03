846471b36fabda67c55ef0c896ff49c1f961ca60
Sergey Arefiev <40056618+sergo2048@users.noreply.github.com>
2020-04-04 02:35:15 +0300

    Add class MyClient, and start work with it

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import discord
+from settings import bot_token
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('We have logged in as {0.user}'.format(client))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+
+client = MyClient()
+client.run(bot_token)
